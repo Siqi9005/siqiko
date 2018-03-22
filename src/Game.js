82ed59eb41e28904bd6f7dc7dbad5634ec4fba95
siqi <liqiangmin@hotmail.com>
2018-03-22 12:17:53 -0700

    123
    123

--- src/Game.js
+++ src/Game.js
@@ -123,21 +123,20 @@ setInterval(this.randomMove,1200);
       else if(this.state.mode === 1){
                 
             comp = (
-                <div id="div"></div>
-                  <div id="background"> 
-
- 
-<img id="img" onselectstart="return false;" unselectable="on" style="-moz-user-select:none;" ondragstart="return false;"> 
-     
-</div> 
-                    <div id="user">Name: {this.state.username}</div>
-               
+                <div id="div">              
+                           <div id="user">Name: {this.state.username}  
+                            </div>
+            
                          <p id="num" class="text">Score:{this.state.usersScore} 0</p>  
+            
 
                             <img ref="random" className="image1" src={this.state.img1} onMouseDown= {this.handleImage} onMouseUp= {this.handleImage2} 
                 
                 height={150} />
+                
 
+
+                 </div>
         );   
             
             
